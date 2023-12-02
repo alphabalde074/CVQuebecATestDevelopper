74598bef91c0dc1f1dd56843a60d9e6859c28edf
Mamadou Alpha Balde <alphabalde074@gmail.com>
2023-12-02 12:14:24 +0530

    Adding C

--- Fujitsu.CvQc.API/Domains/JobController.cs
+++ Fujitsu.CvQc.API/Domains/JobController.cs
@@ -13,7 +13,7 @@ public class JobController : ControllerBase
     public async Task<ActionResult<IEnumerable<JobModel>>> GetJobs(string projectId)
     {
         var task = Task.Run(() => jobService.GetJobs("ProjectId", projectId));
-        int bb = 0;
+        int cc = 0;
         return await task;
     }
 
